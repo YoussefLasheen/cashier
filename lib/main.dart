dd10c950079a92b2afc397df8beb08ab05b1af7a
Youssef Lasheen <youssef@lasheen.dev>
2023-07-24 22:05:04 +0300

    Use material 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:cashier/app/home/receipt_maker/receipt_maker_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
     return MaterialApp(
-      home: ScannerPage(),
+      theme: ThemeData.light(useMaterial3: true),
+      home: const ScannerPage(),
     );
   }
 }
