4d30fe3c0c0defe779091cf48e5fb2f517a179d7
Youssef Lasheen <youssef@lasheen.dev>
2023-07-29 01:24:10 +0300

    Autodispose the scanner controller provider

--- lib/app/home/models/scanner.dart
+++ lib/app/home/models/scanner.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-final scannerControllerProvider = Provider<MobileScannerController>((ref) {
+final scannerControllerProvider =
+    Provider.autoDispose<MobileScannerController>((ref) {
   return MobileScannerController(
     detectionSpeed: DetectionSpeed.noDuplicates,
     facing: CameraFacing.back,
